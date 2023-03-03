85f0600697e1f997424f79c1c2ff1875079f72e1
Elvis Kariithi Githinji <velgithinji@gmail.com>
2023-03-03 14:43:21 +0300

    Revert "Add sys admin model"

--- app/models/sys_admin.rb
+++
@@ -1,2 +0,0 @@
-class SysAdmin < ApplicationRecord
-end
--- db/migrate/20230303091501_create_sys_admins.rb
+++
@@ -1,8 +0,0 @@
-class CreateSysAdmins < ActiveRecord::Migration[7.0]
-  def change
-    create_table :sys_admins do |t|
-
-      t.timestamps
-    end
-  end
-end
