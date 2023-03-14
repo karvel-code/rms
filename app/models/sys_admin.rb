1b90804650583229ae0507a449b1d90f6e875212
karvel-code <velgithinji@gmail.com>
2023-03-14 08:05:07 +0300

    Add validations for sys_admins

--- app/models/sys_admin.rb
+++ app/models/sys_admin.rb
@@ -1,2 +1,10 @@
 class SysAdmin < ApplicationRecord
+    before_save { email.downcase! }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :first_name,  presence: true, length: { maximum: 25 }
+    validates :last_name,  presence: true, length: { maximum: 25 }
+    validates :email, presence: true, 
+                    length: { maximum: 255 }, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
 end
