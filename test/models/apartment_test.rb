d387cf8b84303573867c30ae2d8f65bf6f7b0568
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-15 10:33:11 +0300

    added tests for apartment validations

--- test/models/apartment_test.rb
+++ test/models/apartment_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class ApartmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test  'an apartment should have a name at creation' do
+    # Arrange
+    apartment = Apartment.create!(name: '')
+    # Act 
+
+    # Assert 
+    refute apartment.valid?, 'Apartment should have a name' 
+  end
+
+  test 'an apartment should have a location at creation' do
+    # Arrange
+    apartment = Apartment.create!(name: 'Apartment1', location: '')
+
+    # Act
+    # Assert 
+    refute apartment.valid?, 'Apartment should have a location'
+  end
+
+  test 'apartment should have a contact person at creation' do
+    apartment = Apartment.create!(name: 'Apartment1', location: 'Location1', contact_person: '')
+
+    # Assert 
+    refute apartment.valid?, 'Apartment should have a contact person'
+  end 
 end
