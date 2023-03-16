5f701226f37448b95c35159c68dadc34cbf087a4
karvel-code <velgithinji@gmail.com>
2023-03-16 10:31:13 +0300

    Add current sys admin in sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
 module SessionsHelper
+    #Logs in a given user
     def log_in(sys_admin)
         session[:sys_admin_id] = sys_admin.id
+    end
+
+    #Returns a logged in admin
+    def current_sys_admin
+        if session[:sys_admin_id]
+            @current_sys_admin ||= SysAdmin.find_by(id: session[:sys_admin_id])
+        end
     end
 end
