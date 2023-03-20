9e9a57074ac7e3cc4148c1fca531a13084c5497c
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-20 09:50:54 +0300

    generated unit model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_070037) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_064520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,4 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_070037) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "units", force: :cascade do |t|
+    t.string "floor"
+    t.integer "house_no"
+    t.bigint "apartment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["apartment_id"], name: "index_units_on_apartment_id"
+  end
+
+  add_foreign_key "units", "apartments"
 end
