6537fd6f5135b85e4e098cab2450a2655661dde4
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-20 13:28:35 +0300

    stream to units channel

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,7 @@
 class Unit < ApplicationRecord
   belongs_to :apartment
+
+  after_create_commit -> { broadcast_append_to 'units' }
+  after_update_commit -> { broadcast_replace_to 'units' }
+  after_destroy_commit -> { broadcast_remove_to 'units' }
 end
