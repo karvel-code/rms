e7b3ff201c8a1dad7ce4c2d4c0f99b8a46c21e61
karvel-code <velgithinji@gmail.com>
2023-04-05 16:00:14 +0300

    Add account model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_25_190536) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_123705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "domain"
+    t.string "sub_domain"
+    t.string "name"
+    t.string "email"
+    t.string "address"
+    t.datetime "onboarding_completed_at"
+    t.string "sign_up_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "apartments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
