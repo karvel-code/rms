cb0a401203dbc4812f95a15ac17065fdef061c26
karvel-code <velgithinji@gmail.com>
2023-04-15 09:52:59 +0300

    Add admin model and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admins
+  # devise_for :admins, path: 'admin', path_names: {
+  #   sign_in: 'login',
+  #   sign_out: 'logout',
+  #   password: 'secret',
+  #   confirmation: 'verification',
+  #   unlock: 'unblock',
+  #   registration: 'register',
+  #   sign_up: 'signup'
+  # }
   devise_for :tenants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'apartments#index'
 
-  resources :sys_admins
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
--- db/migrate/20230322111635_add_remember_digest_to_sys_admins.rb
+++
@@ -1,5 +0,0 @@
-class AddRememberDigestToSysAdmins < ActiveRecord::Migration[7.0]
-  def change
-    add_column :sys_admins, :remember_digest, :string
-  end
-end
