f7a15f221881ea96174b545c92507ab84e1e9a76
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-04-15 11:21:20 +0300

    add custom fields to devise for tenants

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
+# rubocop:disable Style/Documentation
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-    include SessionsHelper
+  include SessionsHelper
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u|
+       u.permit(:first_name, :last_name, :phone, :email, :password, :password_confirmation) }
+
+    # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+  end
 end
+
+# rubocop:enable Style/Documentation
