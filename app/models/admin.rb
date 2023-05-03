f0da81ff0b232bae53297db3e04c0edea01547e8
karvel-code <velgithinji@gmail.com>
2023-05-03 09:47:29 +0300

    Scope admin under tenant

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,9 @@
 class Admin < ApplicationRecord
+  acts_as_tenant(:account)
+  
+  # Validations
+  validates_uniqueness_to_tenant :email
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_063726) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_064251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_063726) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
+    t.integer "account_id"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["invitation_token"], name: "index_admins_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_admins_on_invited_by_id"
